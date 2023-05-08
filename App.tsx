cb13a075ff6ceb7b3bd68607b001c6b4a68d2c50
TeddyGavi <matc.davis@gmail.com>
2023-05-08 13:25:19 -0700

    working through tutorial and adding battery level read stream and ui display

--- App.tsx
+++ App.tsx
@@ -1,22 +1,114 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import DeviceModal from "./DeviceConnectionModal";
+import { PulseIndicator } from "./PulseIndicator";
+import useBle from "./useBle";
 
-export default function App() {
+const App = () => {
+  const {
+    requestPermissions,
+    scanForPeripherals,
+    allDevices,
+    connectToDevice,
+    connectedDevice,
+    heartRate,
+    disconnectFromDevice,
+    batteryLevel,
+  } = useBle();
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const scanForDevices = async () => {
+    const isPermissionsEnabled = await requestPermissions();
+    if (isPermissionsEnabled) {
+      scanForPeripherals();
+    }
+  };
+
+  const hideModal = () => {
+    setIsModalVisible(false);
+  };
+
+  const openModal = async () => {
+    scanForDevices();
+    setIsModalVisible(true);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>
-        Open up App.tsx to start working on your app! Test Of hot reload
-      </Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.heartRateTitleWrapper}>
+        {connectedDevice ? (
+          <>
+            <PulseIndicator />
+            <Text style={styles.heartRateTitleText}>Your Heart Rate Is:</Text>
+            <Text style={styles.heartRateText}>{heartRate} bpm</Text>
+            <Text style={styles.heartRateTitleText}>Battery Level is:</Text>
+            <Text style={styles.heartRateText}>{batteryLevel} %</Text>
+          </>
+        ) : (
+          <Text style={styles.heartRateTitleText}>
+            Please Connect to a Heart Rate Monitor
+          </Text>
+        )}
+      </View>
+      <TouchableOpacity
+        onPress={connectedDevice ? disconnectFromDevice : openModal}
+        style={styles.ctaButton}
+      >
+        <Text style={styles.ctaButtonText}>
+          {connectedDevice ? "Disconnect" : "Connect"}
+        </Text>
+      </TouchableOpacity>
+      <DeviceModal
+        closeModal={hideModal}
+        visible={isModalVisible}
+        connectToPeripheral={connectToDevice}
+        devices={allDevices}
+        scan={scanForPeripherals}
+      />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "#f2f2f2",
+  },
+  heartRateTitleWrapper: {
+    flex: 1,
     justifyContent: "center",
+    alignItems: "center",
+  },
+  heartRateTitleText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginHorizontal: 20,
+    color: "black",
+  },
+  heartRateText: {
+    fontSize: 25,
+    marginTop: 15,
+  },
+  ctaButton: {
+    backgroundColor: "#FF6060",
+    justifyContent: "center",
+    alignItems: "center",
+    height: 50,
+    marginHorizontal: 20,
+    marginBottom: 5,
+    borderRadius: 8,
+  },
+  ctaButtonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
   },
 });
+
+export default App;
---
+++ useBle.ts
@@ -0,0 +1,241 @@
+import { useMemo, useState } from "react";
+import {
+  BleError,
+  BleManager,
+  Characteristic,
+  Device,
+} from "react-native-ble-plx";
+import { PermissionsAndroid, Platform } from "react-native";
+import * as ExpoDevice from "expo-device";
+import base64 from "react-native-base64";
+
+const HEART_RATE_SERVICEID = "0000180d-0000-1000-8000-00805f9b34fb";
+const HEART_RATE_CHARACTERISTICID = "00002a37-0000-1000-8000-00805f9b34fb";
+
+const BATTERY_LEVEL_SERVICE = "0000180f-0000-1000-8000-00805f9b34fb";
+const BATTERY_LEVEL_CHAR = "00002a19-0000-1000-8000-00805f9b34fb";
+
+interface BluetoothLowEnergyApi {
+  requestPermissions(): Promise<boolean>;
+  scanForPeripherals(): void;
+  allDevices: Device[];
+  connectToDevice: (deviceId: Device) => Promise<void>;
+  connectedDevice: Device | null;
+  disconnectFromDevice: () => void;
+  heartRate: number;
+  batteryLevel: number;
+}
+
+function useBle(): BluetoothLowEnergyApi {
+  const bleManager = useMemo(
+    () => new BleManager(),
+    // {
+    //   restoreStateIdentifier: "BleInTheBackground",
+    //   restoreStateFunction: (restoredState) => {
+    //     if (restoredState === null) {
+    //       // BleManager was constructed for the first time.
+    //       return;
+    //     } else {
+    //       // BleManager was restored. Check `restoredState.connectedPeripherals` property.
+    //       // array of peripherals
+    //       const restoredDevices = restoredState.connectedPeripherals;
+    //       const device = restoredDevices.filter((device) =>
+    //         device.name?.includes("zephyr")
+    //       );
+    //       setConnectedDevice(device[0]);
+    //     }
+    //   },
+    // }),
+    []
+  );
+
+  const [allDevices, setAllDevices] = useState<Device[]>([]);
+  const [connectedDevice, setConnectedDevice] = useState<Device | null>(null);
+  const [heartRate, setHeartRate] = useState<number>(0);
+  const [batteryLevel, setBatteryLevel] = useState<number>(0);
+
+  const requestAndroid31Permissions = async () => {
+    const bluetoothScanPermissions = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+      {
+        title: "Scan Permission",
+        message: "app requires Bluetooth Scanning",
+        buttonPositive: "Ok",
+      }
+    );
+    const bluetoothConnectPermissions = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+      {
+        title: "Scan Permission",
+        message: "app requires Bluetooth Connecting",
+        buttonPositive: "Ok",
+      }
+    );
+    const bluetoothFineLocationPermissions = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: "Scan Permission",
+        message: "app requires fine location",
+        buttonPositive: "Ok",
+      }
+    );
+
+    return (
+      bluetoothScanPermissions === "granted" &&
+      bluetoothConnectPermissions == "granted" &&
+      bluetoothFineLocationPermissions === "granted"
+    );
+  };
+
+  const requestPermissions = async () => {
+    if (Platform.OS === "android") {
+      if ((ExpoDevice.platformApiLevel ?? -1) < 31) {
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+          {
+            title: "Location Permission",
+            message: "App requires location",
+            buttonPositive: "Ok",
+          }
+        );
+        return granted === PermissionsAndroid.RESULTS.GRANTED;
+      } else {
+        const isAndroid31PermissionsGranted =
+          await requestAndroid31Permissions();
+        return isAndroid31PermissionsGranted;
+      }
+    } else {
+      return true;
+    }
+  };
+
+  const isDuplicateDevice = (devices: Device[], nextDevice: Device) => {
+    return devices.findIndex((device) => nextDevice.id === device.id) > -1;
+  };
+
+  const scanForPeripherals = () => {
+    console.log("scanning");
+    bleManager.startDeviceScan(null, null, (error, device) => {
+      if (error) {
+        console.log(error);
+      }
+      if (device && device.name?.includes("Zephyr")) {
+        setAllDevices((prev) => {
+          if (!isDuplicateDevice(prev, device)) {
+            return [...prev, device];
+          } else {
+            return prev;
+          }
+        });
+      }
+    });
+  };
+
+  const connectToDevice = async (device: Device) => {
+    try {
+      const deviceConnection = await bleManager.connectToDevice(device.id);
+      setConnectedDevice(deviceConnection);
+      await deviceConnection.discoverAllServicesAndCharacteristics();
+      bleManager.stopDeviceScan();
+      startStreamingData(deviceConnection);
+      streamBattery(deviceConnection);
+    } catch (error) {
+      console.log("ERROR IN CONNECTION", error);
+    }
+  };
+
+  const onHeartRateUpdate = (
+    error: BleError | null,
+    characteristic: Characteristic | null
+  ) => {
+    if (error) {
+      console.log(error);
+    } else if (!characteristic?.value) {
+      console.log("No Data Received");
+    } else {
+      const rawData = base64.decode(characteristic.value);
+      let innerHeartRate: number = -1;
+
+      const firstBitValue: number = Number(rawData) & 0x01;
+
+      if (firstBitValue === 0) {
+        innerHeartRate = rawData[1].charCodeAt(0);
+      } else {
+        innerHeartRate =
+          Number(rawData[1].charCodeAt(0) << 8) +
+          Number(rawData[2].charCodeAt(2));
+      }
+
+      setHeartRate(innerHeartRate);
+    }
+  };
+
+  const onBatteryUpdate = (
+    error: BleError | null,
+    characteristic: Characteristic | null
+  ) => {
+    if (error) {
+      console.log(error);
+    } else if (!characteristic?.value) {
+      console.log("No Data Received");
+    } else {
+      let batteryLevel = 0;
+      const raw = base64.decode(characteristic.value);
+      const first: number = Number(raw) & 0x01;
+
+      if (first === 0) {
+        console.log(raw.charCodeAt(0));
+        batteryLevel = raw.charCodeAt(0);
+      } else {
+        batteryLevel = 0;
+      }
+      setBatteryLevel(batteryLevel);
+    }
+  };
+
+  const startStreamingData = (device: Device) => {
+    if (device) {
+      device.monitorCharacteristicForService(
+        HEART_RATE_SERVICEID,
+        HEART_RATE_CHARACTERISTICID,
+        onHeartRateUpdate
+      );
+    } else {
+      console.log("no device connected");
+    }
+  };
+
+  const streamBattery = (device: Device) => {
+    if (device) {
+      device.monitorCharacteristicForService(
+        BATTERY_LEVEL_SERVICE,
+        BATTERY_LEVEL_CHAR,
+        onBatteryUpdate
+      );
+    } else {
+      console.log("no device connected");
+    }
+  };
+
+  const disconnectFromDevice = () => {
+    if (connectedDevice) {
+      bleManager.cancelDeviceConnection(connectedDevice.id);
+      setConnectedDevice(null);
+      setHeartRate(0);
+      // console.log(allDevices);
+    }
+  };
+
+  return {
+    scanForPeripherals,
+    requestPermissions,
+    allDevices,
+    connectToDevice,
+    connectedDevice,
+    heartRate,
+    disconnectFromDevice,
+    batteryLevel,
+  };
+}
+
+export default useBle;
